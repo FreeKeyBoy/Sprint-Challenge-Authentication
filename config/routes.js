d51b14c19599e39386de86fcbc7d03cf57d019c5
Freekeyboy <jjhutton81@gmail.com>
2019-01-26 09:46:24 -0800

    Set scripts to start for npm server. Implemented register endpoint.

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig.js')
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -6,14 +8,43 @@ module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
   server.get('/api/jokes', authenticate, getJokes);
+  server.get('/',(req,res)=>{res.send('Server Running')})//test if running
 };
 
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 14);
+  creds.password = hash;
+  db('users').insert(creds)
+  .then(userId => {
+    res.status(200).json(userId)
+  })
+  .catch(err => {
+    res.json(err)
+  })
 }
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+  if(creds){
+    db('users')
+    .where({username : creds.username})
+    .then(user => {
+      if(user && bcrypt.compareSync(creds.password,user.password)){
+        const token = generateToken(user)
+        res.status(200).json({Token : token});
+      }else{
+        res.status(401).json({Error : "You shall not pass!"})
+      }
+    })
+    .catch(err =>{
+      res.json({Error : err})
+    })
+  }else{
+    res.send('Must include username and password in request')
+  }
 }
 
 function getJokes(req, res) {
